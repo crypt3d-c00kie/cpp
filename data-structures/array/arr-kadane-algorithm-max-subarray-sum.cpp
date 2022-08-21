80521bcb7dfac8ffa9f6b112c08b09e31b73ed01
crypt3d-c00kie <cookie.crypted@gmail.com>
2022-08-21 12:39:58 +0530

    Add files via upload

---
+++ data-structures/array/arr-kadane-algorithm-max-subarray-sum.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int kadaneMaxSumSubArray(vector<int> &arr){
+    int best = 0,sum = 0;
+    int n = arr.size();
+    for(int i=0;i<n;i++){
+        sum = max(arr[i],sum+arr[i]);
+        best = max(best,sum);
+    }
+    return best;
+}
+int main(){
+    vector<int> given;
+    int nSize = 0;
+
+    cout << "Size of the array :: ";
+    cin >> nSize;
+    cout << "Elements :: ";
+    for(int i=0; i<nSize; i++){
+        int temp;
+        cin >> temp;
+        given.push_back(temp);
+    }
+
+    cout << "Solution :: " << kadaneMaxSumSubArray(given) << endl;
+    return 0;
+}

3ff9141e38f83828933a9e7ec044d04e81583a7b
crypt3d-c00kie <cookie.crypted@gmail.com>
2022-09-13 23:20:01 +0530

    Add files via upload

--- data-structures/tree/avl1.cpp
+++ data-structures/tree/avl1.cpp
@@ -1,8 +1,4 @@
-#include <iostream>
-#include <cstdlib>
-#include <vector>
-#include <algorithm>
-
+#include<bits/stdc++.h>
 #define FAILURE { cout << "Ptr memory allocation failed \n"; exit(1); } // memory allocation failed popup
 using namespace std;
 
@@ -16,99 +12,64 @@ typedef struct node
 
 }NODE;
 
-int getHeightofTree(NODE* root)
-{
+int getHeightofTree(NODE* root){
     if(root == NULL)
         return 0;
-
     return max(getHeightofTree(root->leftSubtree),getHeightofTree(root->rightSubtree))+1;
-
 }
-NODE* getNode(int n)
-{
+NODE* getNode(int n){
     NODE* ptr = NULL;
-
     ptr = (NODE*)malloc(sizeof(struct node));
-    
     if(ptr == NULL)
-    {
         FAILURE;
-    }
     ptr->leftSubtree = NULL;
     ptr->rightSubtree = NULL;
     ptr->nHeight = 1;
     ptr->nData = n;
-
     return ptr;
 }
-
-NODE* insert(NODE* root,int nData)
-{
-    if(root == NULL)
-    {
+NODE* insert(NODE* root,int nData){
+    if(root == NULL){
         NODE* ptr = NULL;
-
         ptr = getNode(nData);
         return ptr;
     }
     
     if(nData < root->nData)
-    {
         root->leftSubtree = insert(root->leftSubtree,nData); 
-    }
     else if(nData >= root->nData)
-    {
         root->rightSubtree = insert(root->rightSubtree,nData);
-    }
     return root;
 }
 
-void inOrderTraverse(NODE* root)
-{
+void inOrderTraverse(NODE* root){
     if(root == NULL)
-    {
         return;
-    }
-
     inOrderTraverse(root->leftSubtree);
-    
     root->nHeight = getHeightofTree(root);
    //cout << root->nData << " | ";
     //cout << "Height : " << root->nHeight << endl; 
     inOrderTraverse(root->rightSubtree);
-    
 }
-void populateBalances(NODE* root)
-{
+void populateBalances(NODE* root){
     if(root == NULL)
-    {
         return;
-    }
 
     populateBalances(root->leftSubtree);
     int lh,rh;
     lh = (root->leftSubtree)?(root->leftSubtree->nHeight):0;
     rh = (root->rightSubtree)?(root->rightSubtree->nHeight):0;
     root->nBalanceFactor = lh-rh;
-
     populateBalances(root->rightSubtree);
-
 }
-void inOrderTraverse2(NODE* root)
-{
+void inOrderTraverse2(NODE* root){
     if (root == NULL)
-    {
         return;
-    }
     bool flag = false;
-
     inOrderTraverse2(root->leftSubtree);
     populateBalances(root);
-
     if((root->nBalanceFactor == 0) || (root->nBalanceFactor == 1) || (root->nBalanceFactor == -1))
-    {
         flag = true;
-    }
     cout << "Node val : " 
          << root->nData 
          << " | Height : " 
@@ -119,11 +80,8 @@ void inOrderTraverse2(NODE* root)
          << flag 
          << endl;
     inOrderTraverse2(root->rightSubtree);
-
-
 }
-int main()
-{   
+int main(){   
     NODE* root = NULL;
     vector<int> v;
     v.push_back(41);
@@ -136,9 +94,7 @@ int main()
    // v.push_back(23);
 
     for(int  i=0;i<v.size();i++)
-    {
         root = insert(root,v[i]);
-    }
     
     cout << "Height = " << getHeightofTree(root) << endl;
     /*root = insert(root,41);
@@ -148,11 +104,8 @@ int main()
     root = insert(root,29);
     root = insert(root,50);
     root = insert(root,26);*/
-
-
     inOrderTraverse(root);
     cout << "inordertraverse2" << endl;
     inOrderTraverse2(root);
-
     return 0;
 }

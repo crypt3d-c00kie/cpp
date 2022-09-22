f0c9a8371e287437f19fc4c6b7c8f17f2e215233
crypt3d-c00kie <cookie.crypted@gmail.com>
2022-09-22 23:32:02 +0530

    Add files via upload

---
+++ legacy/map-count-freq-each.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void countFrequency(vector<int> &given){
+    map<int,int> m1;
+    for(int i=0;i<given.size();i++)
+        m1[given[i]]++;
+    
+    for(auto x : m1)
+        cout << x.first << " :: " << x.second << endl;
+    
+}
+int main(){
+    int n;
+    vector<int> given;
+    cout << "Number of elements :: ";
+    cin >> n;
+    cout << "Input :: ";
+    for(int i=0;i<n;i++){
+        int tmp;
+        cin >> tmp;
+        given.push_back(tmp);
+    }
+        
+    countFrequency(given);
+    return 0;
+}

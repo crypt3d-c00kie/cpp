ae425acee08d335b4f6bce068f26b4221c0262dc
crypt3d-c00kie <cookie.crypted@gmail.com>
2022-10-12 12:18:42 +0530

    Add files via upload

---
+++ algorithms/sort-insertion.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void sortInsertion(vector<int> &given){
+    for(int i=1;i<given.size();i++){
+        int now = given[i];
+        int j = i -1;
+
+        while(j>= 0 and given[j] > now){
+            given[j+1] = given[j];
+            j--;
+        }
+        given[j+1] = now;
+    }
+}
+
+int main(){
+    vector<int> given;
+    int n;
+    cout << "Number of elements :: ";
+    cin >> n;
+    cout << "Given :: ";
+    for(int i=0;i<n;i++){
+        int tmp;
+        cin >> tmp;
+        given.push_back(tmp);
+    }
+    cout << "Sorted Result :: ";
+    sortInsertion(given);
+
+    for(auto x : given)
+        cout << x << " ";
+
+    return 0;
+}

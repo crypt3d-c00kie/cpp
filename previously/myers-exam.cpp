3f5da44a06b9dbfd85adc26de31c7ac69d637599
crypt3d-c00kie <cookie.crypted@gmail.com>
2022-10-16 01:45:52 +0530

    Add files via upload

---
+++ previously/myers-exam.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int minsumSolve(vector<int> &given){
+    int sum = 0;
+    for(int i=0;i<given.size();i++){
+        for(int j=i+1;j<given.size();j++){
+            if(given[i] == given[j]){
+                given[j]++;
+            }
+        }
+    }
+    for(auto x : given)
+        sum += x;
+    return sum;
+}
+
+int main(){
+    int n;
+    vector<int> given;
+
+    cout << "Number of elements : ";
+    cin >> n;
+    cout << "Elements :: ";
+    for(int i=0;i<n;i++){
+        int tmp;
+        cin >> tmp;
+        given.push_back(tmp);
+    }
+
+    cout << "Result :: " << minsumSolve(given);
+    return 0;
+}

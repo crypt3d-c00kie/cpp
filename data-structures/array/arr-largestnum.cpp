3fc71e05fb59124c60e5671e00d50cb56b3eabfa
crypt3d-c00kie <cookie.crypted@gmail.com>
2022-10-17 13:08:47 +0530

    Add files via upload

--- data-structures/array/arr-largestnum.cpp
+++ data-structures/array/arr-largestnum.cpp
@@ -1,30 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int findLargest(int arr[], int n)
+int findLargest(vector<int> &given)
 {
-    int res=0;
-    for (int i=0;i<n;i++)
-        if(arr[i] > arr[res])
+    int res=INT_MIN;
+    for (int i=0;i<given.size();i++)
+        if(given[i] > given[res])
             res = i;
 
-    return arr[res];
+    return given[res];
 }
 int main()
 {
     int nSize;
-    cout << "Number of Elements : ";
+    cout << "Number of Elements :: ";
     cin >> nSize;
-    int nArr[nSize];
-
-    for(int i=0;i<nSize;i++)
-        cin >> nArr[i];
+    vector<int> given;
+    cout << "Given :: ";
+    for(int i=0;i<nSize;i++){
+        int tmp;
+        cin >> tmp;
+        given.push_back(tmp);
+    }
         
 /*  printing array
     for(int i=0;i<nSize;i++)
         cout << nArr[i] << " ";
 */
-    cout << "The largest element in this array is ::  " << findLargest(nArr,nSize) << endl;
+    cout << "The largest element ::  " << findLargest(given) << endl;
 
     return 0;
 }

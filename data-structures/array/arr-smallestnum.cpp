927b2c49aebf2b807d99c8f0b30a9a6fd936c19c
crypt3d-c00kie <cookie.crypted@gmail.com>
2022-10-17 13:26:59 +0530

    Add files via upload

---
+++ data-structures/array/arr-smallestnum.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int findSmallest(vector<int> &given){
+    int res=given[0];
+    for (int i=0;i<given.size();i++)
+        if(res > given[i])
+            res = given[i];
+
+    return res;
+}
+int main()
+{
+    int nSize;
+    cout << "Number of Elements :: ";
+    cin >> nSize;
+    vector<int> given;
+    cout << "Given :: ";
+    for(int i=0;i<nSize;i++){
+        int tmp;
+        cin >> tmp;
+        given.push_back(tmp);
+    }
+    cout << "The largest element ::  " << findSmallest(given) << endl;
+
+    return 0;
+}
